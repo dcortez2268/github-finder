d3d4f47542d4cd73bdec0d36d6b35f80301e5049
Dominick Cortez <jdcortez2268@gmail.com>
2022-08-29 10:07:53 -0700

    configure tailwind and daisyui.

---
+++ r-notes-2.js
@@ -0,0 +1,9 @@
+/*************************************************************************************************
+Course Title: React Front to back 2022
+Author: Traversy Media
+Description: Project 2, Github finder app. Helps us practice implementing 3rd party API and reducers
+*************************************************************************************************/
+
+// install tailwind css with create react app instructions found at https://tailwindcss.com/docs/guides/create-react-app
+// installed daisy UI instructions found at https://daisyui.com/docs/install/
+// daisyUI is the most popular tailwind css component library
