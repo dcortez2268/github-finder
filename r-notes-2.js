d12c5ea3af2a878cdf33e12233fa83125d49d9aa
Dominick Cortez <jdcortez2268@gmail.com>
2022-08-30 13:18:51 -0700

    Add userItem and userResults components.

--- r-notes-2.js
+++ r-notes-2.js
@@ -7,3 +7,7 @@ Description: Project 2, Github finder app. Helps us practice implementing 3rd pa
 // install tailwind css with create react app instructions found at https://tailwindcss.com/docs/guides/create-react-app
 // installed daisy UI instructions found at https://daisyui.com/docs/install/
 // daisyUI is the most popular tailwind css component library
+
+//****************************** */
+//   environment variables
+//****************************** */
